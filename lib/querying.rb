a38a30b6439e9027a7cb535fd009093d85191bc7
james Johnson <2013matthew2013@gmail.com>
2020-04-08 19:34:59 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,11 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  
+  "SELECT authors.name AS author_name, subgenres.name AS subgenre_name FROM series
+  JOIN authors
+  ON series.author_id = authors.id
+  JOIN subgenres
+  ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
